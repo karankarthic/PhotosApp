f7ea6d0be1e8cf94d442590bab0f689d6a67f2de
karankarthic.n <karankarthic.n@zohocorp.com>
2020-03-12 11:08:58 +0530

    Initial Commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Karan Karthic on 12/03/20.
+//  Copyright © 2020 Karan Karthic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
